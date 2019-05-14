f6db1e3bc63d1d52fcd827504b1cf14c99450656
Harish Murali <harish02murali@gmail.com>
2019-05-14 13:15:05 -0500

    added plots

--- plts.py
+++ plts.py
@@ -21,17 +21,16 @@ def compFaa(K, mu, phi, Nk, Nmu, Nphi, apar, aper, f, b1, b2, navg, Vs):
                         Bs[i, j, k, l, m] = Bisp((K[i], K[j], K[k], mu[l], phi[m]), (apar, aper, f, b1, b2), (navg, Vs))
                         Covs[i, j, k, l, m] = CovB((K[i], K[j], K[k], mu[l], phi[m]), (apar, aper, f, b1, b2), (navg, Vs))
                         dBda[i, j, k, l, m] = (Bisp((K[i], K[j], K[k], mu[l], phi[m]), (apar+eps, aper, f, b1, b2), (navg, Vs)) - Bs[i, j, k, l, m]) / eps
-                        dFaa = K[i]* K[j]* K[k]* dk**3* dmu* dphi* dBda[i,j,k,l,m]**2 / Covs[i,j,k,l,m]
+                        dFaa = K[i] * K[j] * K[k] * dk**3 * dmu * dphi * dBda[i,j,k,l,m]**2 / Covs[i,j,k,l,m]
                         if not np.isnan(dFaa):
-                            # print('NaN for k1,k2,k3 = ', K[i], K[j], K[k])
                             faa += dFaa
                         else:
                             notTriangles += 1
-    # print('Number of non triangles: ', notTriangles)
+    print('Number of non triangles: ', notTriangles)
     return faa
 
 mu = np.linspace(-1, 1, 2)
-phi = np.linspace(0, 1, 2)
+phi = np.linspace(0, np.pi, 2)
 apar = 1.01
 aper = 0.99
 f = 0.4
@@ -41,19 +40,24 @@ navg = 0.01
 Vs = 1
 eps = 1e-6
 
-# var = (k1, k2, k3, mu, phi)
 parc = (apar, aper, f, b1, b2)
 
 Kmax, Kmin = (0.3, 1e-3)
-Faa = []
-for Nk in range(10, 50, 10):
-    dk = (Kmax - Kmin) / Nk
-    K = np.arange(Kmin, Kmax, dk)
+Faa, dk = [], []
+for Nk in range(10, 20, 5):
+    dk.append((Kmax - Kmin) / Nk)
     Faa.append(0)
+
+    K = np.arange(Kmin, Kmax, dk[-1])
 
     Faa[-1] = compFaa(K, mu, phi, Nk, len(mu), len(phi), apar, aper, f, b1, b2, navg, Vs)
 
     print(Faa[-1]) 
+
+plt.plot(dk, Faa, '*-')
+plt.xlabel('$\Delta k$')
+plt.ylabel('$F_{aa}$')
+plt.show()
 
 # plt.plot(Ks, B1s, label='Bispectrum'); plt.xlabel('k1');plt.ylabel('Bisp(k1, 0.25, 0.25,pi/15,pi/15)'), plt.legend()
 # plt.show()
