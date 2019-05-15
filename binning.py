b4edc40daf64d3d8508a9760fdb33bb908818a2d
Harish Murali <harish02murali@gmail.com>
2019-05-15 12:44:38 -0500

    binning works

--- binning.py
+++ binning.py
@@ -1,23 +1,27 @@
 import matplotlib.pyplot as plt     
-from numba import jit
 from BFisherutils import *
-from scipy.ndimage import convolve1d
 import pdb
 
-def compFaa(K, mu, Nk, Nmu, apar, aper, f, b1, b2, navg, Vs, bin_size=5):
+
+def compFaa(K, mu, Nk, Nmu, apar, aper, f, b1, b2, navg, Vs, bin_size=8):
+    # Nk = Nk - Nk%bin_size
+    # K = K[:Nk]
     dk = K[1] - K[0]
     dmu = mu[1] - mu[0]
+    faa, faa_t = 0, 0
     eps = 1e-6
     Ps = np.zeros((Nk, Nmu), dtype=np.float64)
     dPda = np.zeros((Nk,Nmu), dtype=np.float64)
     Covs = np.zeros((Nk,Nmu), dtype=np.float64)
-    faa, faa_t = 0, 0
 
     Nk_t = Nk//bin_size
     A = conv1d_matrix((Nk_t, Nk), np.ones(bin_size)/bin_size)
     K_t = np.dot(A, K)
+    dk_t = K_t[1] - K_t[0]
     CovP_t = np.zeros((Nk_t, Nk_t, Nmu))
     Fp_t = np.zeros((Nk_t, Nk_t, Nmu))
+    CovP_tdiag = np.zeros((Nk_t, Nmu))
+    Fp_tdiag = np.zeros((Nk_t, Nmu))
     dP_tda = np.zeros((Nk//bin_size, Nmu))
 
     for i in range(Nk):
@@ -27,19 +31,23 @@ def compFaa(K, mu, Nk, Nmu, apar, aper, f, b1, b2, navg, Vs, bin_size=5):
             dPda[i,l] = (Pk((K[i], mu[l]), (apar+eps, aper, f, b1, b2)) - Ps[i,l]) / eps
             faa += K[i]**2 * dk * dmu * dPda[i,l]**2 / Covs[i,l]
 
+    # Computing the same quantities for binned data
     for i in range(Nmu):
+        dP_tda[:, i] = np.dot(A, dPda[:,i])
         CovP_t[:, :, i] = np.dot(A, np.dot(np.diag(Covs[:,i]), A.T))
         Fp_t[:,:,i] = np.linalg.inv(CovP_t[:, :, i])
-        dP_tda[:, i] = np.dot(A, dPda[:,i])
-    
+        CovP_tdiag[:, i] = np.diagonal(CovP_t[:,:,i])
+        Fp_tdiag[:, i] = np.diagonal(Fp_t[:,:,i])
+    # pdb.set_trace()
     for i in range(Nk//bin_size):
-        for j in range(Nk//bin_size):
-            for k in range(Nmu):
-                faa_t += K_t[i]*dmu*K_t[j]*(K_t[1]-K_t[0])*dP_tda[i, k] * dP_tda[j, k] * Fp_t[i, j, k]
-    
-    return faa, faa_t
+        for k in range(Nmu):
+            faa_t += K_t[i]**2 * dk_t * dmu * dP_tda[i, k]**2 * Fp_tdiag[i, k]
+            # faa_t += 2*np.pi*K_t[i]**2*K_t[j]**2*dk_t**2*dmu*dP_tda[i, k] * dP_tda[j, k] * Fp_t[i, j, k]
+
+    return faa, faa_t/bin_size
 
 def conv1d_matrix(shape, kernel):
+    # shape[0] * len(kernel) == shape[1]
     A = np.zeros(shape)
     for i in range(shape[0]):
         A[i, i*len(kernel):(i+1)*len(kernel)] = kernel
@@ -54,26 +62,31 @@ b2 = 0
 navg = 0.01
 Vs = 1
 eps = 1e-6
+Kmax, Kmin = (0.8, 1e-3)
 
 parc = (apar, aper, f, b1, b2)
 
-Kmax, Kmin = (0.8, 1e-3)
-Faa, dk, Faa_t = [], [], []
-for Nk in range(10, 30, 2):
-    dk.append((Kmax - Kmin) / Nk)
-    Faa.append(0)
-    Faa_t.append(0)
+Nks = np.arange(16, 64, 8)
+bin_sizes = np.array([2, 4, 8])
+dk = (Kmax - Kmin) / Nks
 
-    K = np.arange(Kmin, Kmax, dk[-1])
+Faa = np.zeros(len(Nks))
+Faa_t = np.zeros((len(Nks), len(bin_sizes)))
 
-    Faa[-1], Faa_t[-1] = compFaa(K, mu, Nk, len(mu), apar, aper, f, b1, b2, navg, Vs)
+for i in range(len(Nks)):
+    for j in range(len(bin_sizes)):
+        K = np.arange(Kmin, Kmax, dk[i])
 
-    print(Faa[-1], Faa_t[-1]) 
+        Faa[i], Faa_t[i, j] = compFaa(K, mu, Nks[i], len(mu), apar, aper, f, b1, b2, navg, Vs, bin_sizes[j])
+
+        print(Faa[i], Faa_t[i, j]) 
 
 plt.plot(dk, Faa, '*-', label='$F_{aa}$')
-plt.plot(dk, Faa_t, '*-', label=r'$\tildeF_{aa}$')
+for i in range(len(bin_sizes)):
+    plt.plot(dk, Faa_t[:, i], '*-', label=r'$\tildeF_{aa}$, bin size='+str(bin_sizes[i]))
 plt.xlabel('$\Delta k$')
 plt.ylabel('$F_{aa}$')
 plt.legend()
+plt.savefig('./plot_bins.png')
 plt.show()
 
