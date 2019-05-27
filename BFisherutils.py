738cc255c5d9dba1ac18bf2751513defc9d775a7
Harish Murali <harish02murali@gmail.com>
2019-05-27 14:59:24 -0500

    vectorised PS code

--- BFisherutils.py
+++ BFisherutils.py
@@ -30,6 +30,22 @@ def Pk_vec(var, parc):
     return A*(0.64*Psmooth_vec + am2/K**2 + am1/K + a0 + a1*K + a2*K**2)*Pbao_vec
 
 
+def Pk_aniso(var,parc):
+    '''
+    Anisotropic power spectrum.
+    var = k, mu
+    parc = f, b1
+    '''
+    k, mu = var
+    apar, aper, f, b1, b2 = parc
+
+    k = k.reshape(len(k),1)
+    mu = mu.reshape(len(mu),1)
+
+    k = np.dot(k, np.sqrt(aper**2+mu.T**2*(apar**2-aper**2)))
+    mu = mu*apar/np.sqrt(aper**2+mu**2*(apar**2-aper**2))
+    return (b1 + f*mu.T)**2*Piso(k)
+
 def Pk(var,parc):
     '''
     Anisotropic power spectrum.
@@ -42,6 +58,78 @@ def Pk(var,parc):
     k = k*np.sqrt(aper**2+mu**2*(apar**2-aper**2))
     mu = mu*apar/np.sqrt(aper**2+mu**2*(apar**2-aper**2))
     return (b1 + f*mu)**2*Piso(k)
+
+
+def Bisp_vec(var,parc,pars):
+    '''
+    Bispectrum as a function of 5 variables - k1,k2,k3,mu1,phi12 and cosmological
+    parameters parc.
+    This doesn't check for triangular condition.
+    '''
+
+    k1_v, k2_v, k3_v, mu1_v, phi12_v = var
+    apar, aper, f, b1, b2 = parc
+    nave, Vs = pars
+    lk1, lk2, lk3, lmu, lphi = len(k1_v), len(k2_v), len(k3_v), len(mu1_v), len(phi12_v)
+
+    k1 = k1_v.reshape(lk1,1,1,1,1) * np.ones((lk1,lk2,lk3,lmu,lphi))
+    k2 = k2_v.reshape(1,lk2,1,1,1) * np.ones((lk1,lk2,lk3,lmu,lphi))
+    k3 = k3_v.reshape(1,1,lk3,1,1) * np.ones((lk1,lk2,lk3,lmu,lphi))
+    mu1 = mu1_v.reshape(1,1,1,lmu,1) * np.ones((lk1, lk2, lk3, lmu, lphi))
+    phi12 = phi12_v.reshape(1,1,1,1,lphi) * np.ones((lk1, lk2, lk3, lmu, lphi))
+
+    mu12 = (k3**2 - k1**2 - k2**2)/2/k1/k2
+    mu2 = mu1*mu12 - np.sqrt(1 - mu1**2)*np.sqrt(1 - mu12**2)*np.cos(phi12)
+    mu3 = -(mu1*k1 + mu2*k2)/k3
+
+    # rescale for AP
+    k1 = k1*np.sqrt(aper**2+mu1**2*(apar**2-aper**2))
+    k2 = k2*np.sqrt(aper**2+mu2**2*(apar**2-aper**2))
+    k3 = k3*np.sqrt(aper**2+mu3**2*(apar**2-aper**2))
+    mu1 = mu1*apar/np.sqrt(aper**2+mu1**2*(apar**2-aper**2))
+    mu2 = mu2*apar/np.sqrt(aper**2+mu2**2*(apar**2-aper**2))
+    mu3 = mu3*apar/np.sqrt(aper**2+mu3**2*(apar**2-aper**2))
+
+    mu12 = (k3**2 - k1**2 - k2**2)/2/k1/k2
+    mu31 = -(k1 + k2*mu12)/k3
+    mu23 = -(k1*mu12 + k2)/k3
+
+    k12 = np.sqrt(k1**2 + k2**2 + 2*k1*k2*mu12)
+    k23 = np.sqrt(k2**2 + k3**2 + 2*k2*k3*mu23)
+    k31 = np.sqrt(k3**2 + k1**2 + 2*k3*k1*mu31)
+
+    Z1k1 = b1 + f*mu1**2
+    Z1k2 = b1 + f*mu2**2
+    Z1k3 = b1 + f*mu3**2
+
+    F12 = 5./7. + mu12/2*(k1/k2 + k2/k1) + 2./7.*mu12**2
+    F23 = 5./7. + mu23/2*(k2/k3 + k3/k2) + 2./7.*mu23**2
+    F31 = 5./7. + mu31/2*(k3/k1 + k1/k3) + 2./7.*mu31**2
+
+    G12 = 3./7. + mu12/2*(k1/k2 + k2/k1) + 4./7.*mu12**2
+    G23 = 3./7. + mu23/2*(k2/k3 + k3/k2) + 4./7.*mu23**2
+    G31 = 3./7. + mu31/2*(k3/k1 + k1/k3) + 4./7.*mu31**2
+
+    mu1p2 = (mu1*k1 + mu2*k2)/k12
+    mu2p3 = (mu2*k2 + mu3*k3)/k23
+    mu3p1 = (mu3*k3 + mu1*k1)/k31
+
+    Z2k12 = b2/2. + b1*F12 + f*mu1p2**2*G12
+    Z2k12 += f*mu1p2*k12/2.*(mu1/k1*Z1k2 + mu2/k2*Z1k1)
+    Z2k23 = b2/2. + b1*F23 + f*mu2p3**2*G23
+    Z2k23 += f*mu2p3*k23/2.*(mu2/k2*Z1k3 + mu3/k3*Z1k2)
+    Z2k31 = b2/2. + b1*F31 + f*mu3p1**2*G31
+    Z2k31 += f*mu3p1*k31/2.*(mu3/k3*Z1k1 + mu1/k1*Z1k3)
+
+    Bi = 2*Z2k12*Z1k1*Z1k2*Piso(k1)*Piso(k2)
+    Bi += 2*Z2k23*Z1k2*Z1k3*Piso(k2)*Piso(k3)
+    Bi += 2*Z2k31*Z1k3*Z1k1*Piso(k3)*Piso(k1)
+    '''
+    I think I should add this.
+    '''
+    # Bi += (Pk([k1_v,mu1_v],parc) + Pk([k2_v,mu2_v],parc) + Pk([k3_v,mu3_v],parc))/nave
+
+    return Bi
 
 def Bisp(var,parc,pars):
     '''
--- plts.py
+++ plts.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt     
+import matplotlib.pyplot as plt
 from numba import jit
 from BFisherutils import *
 import pdb
